31fb271b25b224e56902766592648221c9b507fd
Jean Carlos Reyes Encarnacion <yacalo45@hotmail.es>
2021-11-02 09:42:01 -0400

    Add the first part of the proyect

---
+++ BusinessLayer/CustomComboBoxItem/ComboBoxItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.ComboBoxItem
+{
+    public class ComboBoxItem
+    {
+
+        public string Text { get; set; }
+        public int Value { get; set; }
+    }
+}
---
+++ LoanCalculator/FrmHome.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace LoanCalculator
+{
+    partial class FrmHome
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnPrincipal = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pnPrincipal
+            // 
+            this.pnPrincipal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(217)))), ((int)(((byte)(170)))));
+            this.pnPrincipal.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.pnPrincipal.Name = "pnPrincipal";
+            this.pnPrincipal.Size = new System.Drawing.Size(652, 37);
+            this.pnPrincipal.TabIndex = 0;
+            this.pnPrincipal.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnPrincipal_MouseDown);
+            // 
+            // FrmHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.ClientSize = new System.Drawing.Size(652, 472);
+            this.Controls.Add(this.pnPrincipal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmHome";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnPrincipal;
+    }
+}
+
